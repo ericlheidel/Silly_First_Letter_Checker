b448e6001ab708ecef3994848046fd2e8ed15d3c
Heidel <136027069+heidelpants@users.noreply.github.com>
2023-08-23 09:06:26 -0500

    Silly_First_Letter_Checker
    Initial Upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const inputText = document.getElementById("input-text")
+const inputDiv = document.getElementById("input-div")
+const letterSelect = document.getElementById("letter-select")
+
+
+function doesMyNameStartWith() {
+    const name = inputEl.value
+    if (name.charAt(0) === letterSelect.value.toUpperCase() || name.charAt(0) === letterSelect.value.toLowerCase()) {
+        inputText.textContent = `YES, You Name starts with a(n) "${letterSelect.value}"`
+    } else {
+        inputText.textContent = `NO, your name DOES NOT start with a(n) "${letterSelect.value}"`
+    }
+}
+
+function reset() {
+    inputEl.value = ""
+    inputText.textContent = ""
+    letterSelect.value = "A"
+}
