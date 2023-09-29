445401ad5686cb3d7dcac7945010d1c37e6fa7be
Heidel <136027069+heidelpants@users.noreply.github.com>
2023-09-29 15:23:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const letterSelect = document.getElementById("letter-select")
 function doesMyNameStartWith() {
     const name = inputEl.value
     if (name.charAt(0) === letterSelect.value.toUpperCase() || name.charAt(0) === letterSelect.value.toLowerCase()) {
-        inputText.textContent = `YES, You Name starts with a(n) "${letterSelect.value}"`
+        inputText.textContent = `YES, your name starts with a(n) "${letterSelect.value}"`
     } else {
         inputText.textContent = `NO, your name DOES NOT start with a(n) "${letterSelect.value}"`
     }
